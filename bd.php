289090141c9b51a6f0f6ab532835a812b19cf917
MaximZamotaev228 <112868632+MaximZamotaev228@users.noreply.github.com>
2023-05-30 13:44:39 +0300

    Update bd.php

--- bd.php
+++ bd.php
@@ -1,7 +1,7 @@
 <?php
     function db() {
-        $user='u52877';
-        $pass='5248088';
-        return new PDO ("mysql:host=localhost;dbname=u52877", $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        $user='u52876';
+        $pass='9106944';
+        return new PDO ("mysql:host=localhost;dbname=u52876", $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
       }
 ?>
